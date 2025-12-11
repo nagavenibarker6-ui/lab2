f32efcd5bef839ee83ac8965162657c3b60e1e42
Nagaveni.Barker <nagavenibarker6@gmail.com>
2025-12-11 21:27:43 +0530

    Update and rename README.md to product.py

--- README.md
+++
@@ -1,2 +0,0 @@
-# lab2
-This folder has pytest.
---
+++ product.py
@@ -0,0 +1,11 @@
+def product_info(product_id, name, quantity, price):
+    return (
+        f"Product Details:\n"
+        f"ID: {product_id}\n"
+        f"Name: {name}\n"
+        f"Quantity: {quantity}\n"
+        f"Price: {price}"
+    )
+if __name__ == "__main__":
+    print(product_info(101, "Laptop", 5, 55000))
+
